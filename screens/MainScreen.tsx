b85f7b814f836975a14278acd8098f7ea065257c
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2023-02-23 16:57:14 +0900

    Refactor : MainScreen test label deleted

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Shadow } from 'react-native-shadow-2'
 import {View, Text, StyleSheet, Image} from "react-native";
 
 function MainScreen() {
     return (
         <View style={styles.container}>
-            <Text>MainScreen</Text>
+
             <Image source = {require('../images/logo.jpg')} style = {styles.logoImage}  />
         </View>
     );
@@ -16,15 +15,14 @@ function MainScreen() {
 const styles = StyleSheet.create({
     container :{
         flex:1,
-        alignItems:'center',
-        justifyContent:'center',
+        alignItems : 'center',
         backgroundColor : '#ffffff',
     },
 
     logoImage : {
         resizeMode : 'stretch',
         width: 100,
-        height: 100,
+        height: 80,
         backgroundColor: '(0,0,0,0.5)',
     },
 })

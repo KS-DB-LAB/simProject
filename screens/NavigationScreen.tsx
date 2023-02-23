bed38bb339e463eb55582c40134b0b4801d0790b
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2023-02-23 16:58:10 +0900

    Feat : Imported and added 'SalesAndProfitScreen' in NavigationScreen.tsx

--- screens/NavigationScreen.tsx
+++ screens/NavigationScreen.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import 'react-native-gesture-handler'
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
 import MainScreen from "./MainScreen";
 import LoginScreen from "./LoginScreen";
-import {StyleSheet} from "react-native";
+import SalesAndProfitScreen from "./SalesAndProfitScreen";
+
 
 const Stack = createStackNavigator();
 function NavigationScreen() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="LoginScreen" screenOptions={{headerShown : false}}>
+            <Stack.Navigator initialRouteName="SalesAndProfitScreen" screenOptions={{headerShown : false}}>
                 <Stack.Screen name="MainScreen" component={MainScreen}/>
+                <Stack.Screen name="SalesAndProfitScreen" component={SalesAndProfitScreen}/>
                 <Stack.Screen name="LoginScreen" component={LoginScreen}/>
             </Stack.Navigator>
         </NavigationContainer>

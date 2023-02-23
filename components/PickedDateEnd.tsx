bcd7dda864e1c5b14b1b01eced5e6c4c6772fd23
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2023-02-23 23:44:44 +0900

    Feat : Made selecting date for sales and profit time

--- components/PickedDateEnd.tsx
+++ components/PickedDateEnd.tsx
@@ -1,37 +1,30 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text} from "react-native";
+import {Pressable, StyleSheet, Text, View} from "react-native";
 
 
 
-function PickedDateEnd() {
-
-    const currentDateSet = () => {
-        let now = new Date();
-        let time = {
-            year : now.getFullYear(),
-            month : now.getMonth() > 9 ? now.getMonth() + 1 : '0' + (now.getMonth() + 1) ,
-            day : now.getDate() > 9 ? now.getDate() : '0' + now.getDate(),
-            hours : now.getHours(),
-            minutes : now.getMinutes(),
-        };
-
-        let monthEndDate = {
-            '01' : 31 , '02' : 29, '03' : 31, '04' : 30, '05' : 31, '06' : 30, '07' : 31,
-            '08' : 30, '09' : 31, '10' : 30, '11' : 31, '12' : 30,
-        }
-
-        return `${time.year}.${time.month}.${monthEndDate[time.month]}`;
-    }
-
-
-    return <Text style ={styles.dateTextStyle}>{currentDateSet()}</Text>
+function PickedDateEnd({dateString}) {
+    return (
+        <View>
+            <Text style={styles.dateTextStyle}>{dateString}</Text>
+        </View>
+    );
 }
 
 
 const styles = StyleSheet.create({
     dateTextStyle : {
         fontSize : 15,
-    }
+    },
+    selectDateOfSalesStyle : {
+        width : 100,
+        height: 25,
+        backgroundColor : "#ffffff",
+        borderRadius : 10,
+        alignItems : 'center',
+        justifyContent : 'center',
+    },
+
 })
 
 export default PickedDateEnd;
--- screens/SalesAndProfitScreen.tsx
+++ screens/SalesAndProfitScreen.tsx
@@ -1,12 +1,64 @@
-import React from "react";
-import {StyleSheet, View, Text, Image, Pressable} from "react-native";
+import React, {useState} from "react";
+import {StyleSheet, View, Text, Image, Pressable, Modal, Alert} from "react-native";
 
 import PickedDateStart from "../components/PickedDateStart";
 import PickedDateEnd from "../components/PickedDateEnd";
 
-import DatePicker from "react-native-date-picker";
+import DatePickerModal from "react-native-modal-datetime-picker";
+
+
 
 function SalesAndProfitScreen(){
+
+
+
+    let now = new Date();
+    let time = {
+        year : now.getFullYear(),
+        month : now.getMonth() > 9 ? now.getMonth() + 1 : '0' + (now.getMonth() + 1) ,
+        day : now.getDate() > 9 ? now.getDate() : '0' + now.getDate(),
+        hours : now.getHours(),
+        minutes : now.getMinutes(),
+    };
+
+    let monthEndDate = {
+        '01' : 31 , '02' : 29, '03' : 31, '04' : 30, '05' : 31, '06' : 30, '07' : 31,
+        '08' : 30, '09' : 31, '10' : 30, '11' : 31, '12' : 30,
+    }
+
+
+    const currentStartDateSet = () => {
+        return `${time.year}.${time.month}.01`;
+    }
+
+    const currentEndDateSet = () => {
+        return `${time.year}.${time.month}.${monthEndDate[time.month]}`;
+    }
+
+    const [selectedDateStartString, setSelectedDateStartString] = useState(currentStartDateSet());
+    const [selectedDateEndString, setSelectedDateEndString] = useState(currentEndDateSet());
+    const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
+
+    const [selectedStartOrEnd, setSelectedStartOrEnd] = useState('start');
+
+    const showDatePicker = (selectStartOrEnd) => {
+        setSelectedStartOrEnd(selectStartOrEnd == 'start' ? 'start' : 'end');
+        setIsDatePickerVisible(true);
+    }
+
+    const hideStartDatePicker = () => {
+        setIsDatePickerVisible(false);
+    }
+
+    const handleConfirmStartDate = (date) => {
+        const year = date.getFullYear();
+        const month = date.getMonth()>  9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1) ;
+        const day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
+        const temporaryDateString = `${year}.${month}.${day}`
+        selectedStartOrEnd == 'start' ? setSelectedDateStartString(temporaryDateString) : setSelectedDateEndString(temporaryDateString)
+        hideStartDatePicker();
+    }
+
 
     return(
         <View style={styles.container}>
@@ -17,18 +69,29 @@ function SalesAndProfitScreen(){
             </View>
 
             <View style = {styles.dateSelectBoxContainerStyle}>
-                <Pressable style = {styles.selectDateOfSalesStyle}>
-                    <PickedDateStart />
+                <Pressable onPress={() => showDatePicker('start')} style = {styles.selectDateOfSalesStyle}>
+                    <PickedDateStart dateString={selectedDateStartString}/>
                 </Pressable>
                 <Text style ={styles.salesDateFromAndToSignStyle}>~</Text>
-                <Pressable style = {styles.selectDateOfSalesStyle}>
-                    <PickedDateEnd />
+                <Pressable onPress={() => showDatePicker('end')} style = {styles.selectDateOfSalesStyle}>
+                    <PickedDateEnd dateString={selectedDateEndString}/>
                 </Pressable>
             </View>
-        </View>
-    )
 
+            <DatePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleConfirmStartDate}
+                onCancel={hideStartDatePicker}
+            />
+            
+        </View>
+
+    )
 }
+
+
+
 
 const styles = StyleSheet.create({
     container :{
@@ -55,7 +118,7 @@ const styles = StyleSheet.create({
     },
 
     dateSelectBoxContainerStyle : {
-        width : 300,
+        width : 350,
         height : 55,
         borderRadius : 10,
         backgroundColor : '#D9D9D9',
@@ -75,8 +138,9 @@ const styles = StyleSheet.create({
 
     salesDateFromAndToSignStyle: {
         padding : 15,
-    }
+    },
 
 })
 
 export default SalesAndProfitScreen;
+

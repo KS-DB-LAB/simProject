248e208b6519ad151a2c334595e38083699c795b
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2023-02-23 16:56:50 +0900

    Refactor : Screen imports deleted

--- App.tsx
+++ App.tsx
@@ -3,8 +3,7 @@ import React from "react";
 import SplashScreen from "./screens/SplashScreen";
 // @ts-ignore
 import NavigationScreen from "./screens/NavigationScreen"
-import MainScreen from "./screens/MainScreen";
-import LoginScreen from "./screens/LoginScreen";
+
 
 
 export default class extends React.Component {

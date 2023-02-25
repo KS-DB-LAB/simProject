ca7af7dafb6d488c0bf05ea2b210beadc5fd0868
JangHongJoon <wkdghdwns19969@gmail.com>
2023-02-25 23:10:52 +0900

    Feat : Made SideMenu.tsx

---
+++ components/SideMenu.tsx
@@ -0,0 +1,107 @@
+import {NavigationContainer} from "@react-navigation/native";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+// @ts-ignore
+import React from 'react';
+
+
+import {Pressable, View, Image, Text,StyleSheet} from 'react-native';
+
+import MainScreen from "../screens/MainScreen";
+import SalesAndProfitScreen from "../screens/SalesAndProfitScreen";
+
+const Drawer = createDrawerNavigator();
+
+
+function CustomDrawerContent({navigation}){
+    const drawerItems =[
+        {
+            label : 'MainScreen',
+            onPress: () => navigation.navigate('MainScreen'),
+        },
+        {
+            label: 'SalesAndProfitScreen',
+            onPress: () => navigation.navigate('SalesAndProfitScreen'),
+        },
+
+    ]
+    return (
+        <View style={styles.drawerBackgroundStyle}>
+            <View style={styles.upperComponentsContainerStyle}>
+                <Pressable onPress={() => navigation.closeDrawer()} style={styles.sideBarCloseIconContainerStyle}>
+                    <Image source = {require('../images/sideBarCloseIcon.jpg')} style = {styles.logoImage} />
+                </Pressable>
+
+            </View>
+            <View style ={styles.drawerButtonsContainerStyle}>
+                {drawerItems.map((item, index) => (
+                    <Pressable key={index} style={styles.drawerButtonStyle} onPress={item.onPress}>
+                        <Text>{item.label}</Text>
+                    </Pressable>
+                ))}
+            </View>
+        </View>
+    )
+}
+
+function SideMenu(){
+    return (
+        <NavigationContainer independent={true} >
+            <Drawer.Navigator
+                initialRouteName="SalesAndProfitScreen"
+                screenOptions={{drawerPosition: 'right', headerShown : false}}
+                drawerContent={props => <CustomDrawerContent {...props}/>}>
+                <Drawer.Screen name="SalesAndProfitScreen" component={SalesAndProfitScreen} />
+                <Drawer.Screen name="MainScreen" component={MainScreen} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    )
+
+}
+
+
+const styles = StyleSheet.create({
+    upperComponentsContainerStyle:{
+        flexDirection : 'row',
+
+        justifyContent : 'flex-end',
+        marginTop: 30,
+    },
+    logoImage: {
+        resizeMode : 'contain',
+        width:35,
+        height:35,
+
+    },
+    sideBarCloseIconContainerStyle:{
+        marginRight:30,
+        marginTop:5,
+    },
+    drawerBackgroundStyle: {
+        flex:1,
+        backgroundColor: '#D9D9D9',
+        alignItems: 'flex-end',
+    },
+    drawerButtonsContainerStyle:{
+        flex:1,
+        backgroundColor: '#D9D9D9',
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    drawerButtonStyle:{
+        width: 200,
+        height: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent:'center',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor : '#B3AFAF',
+        borderRadius : 10,
+        marginTop : 20,
+        marginRight: 35,
+    },
+
+})
+
+
+export default SideMenu;

3d349524b04e6fee43f78614f2fa751086ef2857
JangHongJoon <wkdghdwns19969@gmail.com>
2023-02-27 00:10:34 +0900

    Fix : Fixed side menu bug from MainScreen.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import SplashScreen from "./screens/SplashScreen";
 // @ts-ignore
 import NavigationScreen from "./screens/NavigationScreen"
 
-import 'react-native-url-polyfill/auto';
+import 'react-native-url-polyfill/auto';;
+
 
 export default class extends React.Component {
   state={
@@ -14,13 +15,18 @@ export default class extends React.Component {
       setTimeout(() => {this.setState({isLoading: false})}, 3000);
   }
 
-  render(){
-      if(this.state.isLoading){
-          return <SplashScreen/>
-      }else{
-          return <NavigationScreen/>;
-      }
-  }
+
+
+
+  render() {
+
+        if (this.state.isLoading) {
+            return <SplashScreen/>
+        }
+        else {
+            return <NavigationScreen/>;
+        }
+    }
 }
 
 

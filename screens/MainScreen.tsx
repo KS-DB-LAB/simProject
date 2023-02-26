226c83f0a1bd14337a75c70d6b92c62bc1600c50
Choijake <107412414+Choijake@users.noreply.github.com>
2023-02-26 13:19:43 +0900

    refactor : mainscreen fixed

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,13 +1,67 @@
-// @ts-ignore
 import React from "react";
-import {View, Text, StyleSheet, Image} from "react-native";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import {View, Text, StyleSheet, Image, Button} from "react-native";
+
+
 
 function MainScreen() {
+    {/*TODO : 크롤링으로 가져온 데이터 삽입*/}
     return (
-        <View style={styles.container}>
 
+
+        <View style={styles.container}>
             <Image source = {require('../images/logo.jpg')} style = {styles.logoImage}  />
+            <Text style={styles.cheerText}>점장님을 응원합니다 !</Text>
+            <View style={{top : 30}}>
+                <View style={styles.wholeDash}>
+                    <Text style={styles.wholeText}>예상 가게 매출</Text>
+                    <Text style={styles.wholeSales}> 원</Text>
+                </View>
+
+                <View style={styles.seperateDash}>
+                    <View style={styles.baeminTagColor}></View>
+                    <View style={{left : 50, bottom : 42}}>
+                        <Text style={{fontSize : 18}}>배달의 민족</Text>
+                    </View>
+                    <View style={{left : 200, bottom : 65}}>
+                        <Text style={{fontSize : 18, fontWeight : 'bold'}}>원</Text>
+                    </View>
+                </View>
+
+                <View style={styles.seperateDash}>
+                    <View style={styles.coupangEatsTagColor}></View>
+                    <View style={{left : 50, bottom : 42}}>
+                        <Text style={{fontSize : 18}}>쿠팡 이츠</Text>
+                    </View>
+                    <View style={{left : 200, bottom : 65}}>
+                        <Text style={{fontSize : 18, fontWeight : 'bold'}}>원</Text>
+                    </View>
+                </View>
+
+                <View style={styles.seperateDash}>
+                    <View style={styles.ddangeoyoTagColor}></View>
+                    <View style={{left : 50, bottom : 42}}>
+                        <Text style={{fontSize : 18}}>땡겨요</Text>
+                    </View>
+                    <View style={{left : 200, bottom : 65}}>
+                        <Text style={{fontSize : 18, fontWeight : 'bold'}}>원</Text>
+                    </View>
+                </View>
+
+                <View style={styles.seperateDash}>
+                    <View style={styles.yogiyoTagColor}></View>
+                    <View style={{left : 50, bottom : 42}}>
+                        <Text style={{fontSize : 18}}>요기요</Text>
+                    </View>
+                    <View style={{left : 200, bottom : 65}}>
+                        <Text style={{fontSize : 18, fontWeight : 'bold'}}>원</Text>
+                    </View>
+                </View>
+            </View>
         </View>
+
+
     );
 }
 
@@ -16,16 +70,93 @@ function MainScreen() {
 const styles = StyleSheet.create({
     container :{
         flex:1,
-        alignItems : 'center',
+        alignItems:'center',
+        justifyContent:'center',
         backgroundColor : '#ffffff',
+
     },
 
     logoImage : {
         resizeMode : 'stretch',
         width: 100,
-        height: 80,
+        height: 70,
         backgroundColor: '(0,0,0,0.5)',
+        bottom : 35
     },
+
+    wholeText : {
+        fontSize : 21,
+        color : 'black',
+        position : 'absolute',
+        top : 50,
+        left : 45,
+    },
+
+    wholeSales : {
+        fontSize : 25,
+        fontWeight : 'bold',
+        color : 'black',
+        position : 'absolute',
+        top : 100,
+        left : 45,
+    },
+
+    cheerText : {
+        fontSize : 18,
+        fontWeight : 'bold',
+        color : 'black',
+        position : 'absolute',
+        top : 125,
+        left : 30,
+
+    },
+
+    wholeDash : {
+//         alignItems: 'flex-start',
+//         justifyContent: 'flex-start',
+        backgroundColor : '#D8D8D8',
+        width : 350,
+        height : 200,
+        borderRadius : 7,
+        marginBottom : 12,
+    },
+
+    seperateDash : {
+        //         alignItems: 'flex-start',
+        //         justifyContent: 'flex-start',
+        backgroundColor : '#D8D8D8',
+        width : 350,
+        height : 60,
+        borderRadius : 7,
+        marginBottom : 12,
+
+    },
+
+    baeminTagColor: {
+        width: 40,
+        height: 60,
+        backgroundColor:'#39C5C4',
+        borderRadius : 10,
+    },
+    yogiyoTagColor: {
+        width: 40,
+        height: 60,
+        backgroundColor:'#FA0050',
+        borderRadius : 10,
+    },
+    coupangEatsTagColor: {
+        width: 40,
+        height: 60,
+        backgroundColor: '#31B4DD',
+        borderRadius: 10,
+    },
+    ddangeoyoTagColor:{
+        width: 40,
+        height: 60,
+        backgroundColor: '#FB521C',
+        borderRadius: 10,
+    },
+
 })
 
 export default MainScreen;

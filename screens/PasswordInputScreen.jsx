41703d4337fd9b72d2609fba1a1418842bc736b9
Choijake <107412414+Choijake@users.noreply.github.com>
2023-03-23 02:19:37 +0900

    feat/SP-57,58,59 mostly done

---
+++ screens/PasswordInputScreen.jsx
@@ -0,0 +1,113 @@
+import React, {useState} from "react";
+import {View, Text, StyleSheet, Image, Button, Pressable, TouchableOpacity, Dimensions, TextInput} from "react-native";
+
+
+function PasswordInputScreen({navigation}){
+    return(
+        <View>
+            <View style = {styles.titleContainerStyle}>
+                <Text style ={styles.titleStyle}>비밀번호 입력</Text>
+            </View>
+
+            <View style={styles.PasswordInputContainer}>
+                <Text style ={styles.commentForLogin}>서비스를 사용하려면 로그인하세요.</Text>
+                <TextInput secureTextEntry={true} style={styles.accountInputBox} placeholder="  비밀번호" />
+            </View>
+
+            <View style = {styles.DialogButtonContainer}>
+                <Pressable  style={styles.DialogButtonsStyle}>
+                    <Text>취소</Text>
+                </Pressable>
+                <View style={{ width: 16 }} />
+                <Pressable onPress={()=>navigation.navigate('JoinScreen')} style={styles.DialogButtonsStyle}>
+                    <Text>완료</Text>
+                </Pressable>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container :{
+        flex:1,
+        alignItems : 'center',
+        justifyContent : 'center',
+        backgroundColor : '#ffffff',
+    },
+
+    titleContainerStyle : {
+        paddingTop : 40,
+        paddingBottom : 20,
+        alignSelf:'flex-start',
+    },
+
+    titleStyle : {
+        fontSize : 18,
+        fontWeight : 'bold',
+
+        left : 150,
+        top : 200
+    },
+
+    DialogButtonContainer : {
+        flexDirection: 'row',
+    },
+
+    DialogButtonsStyle : {
+        top : 450,
+        justifyContent:'center',
+        left : 95,
+        alignItems : 'center',
+        //flex : 1,
+        borderRadius : 10,
+        borderWidth:1,
+        borderColor:'black',
+        position : 'relative',
+        width:100,
+        height:36,
+
+
+    },
+
+    PasswordInputContainer : {
+        flex:1,
+        alignItems: 'center',
+        top : 200
+
+
+    },
+    errorModalMessageContainer: {
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    errorModalMessageBox:{
+        width:300,
+        height:200,
+        backgroundColor:"#d9d9d9",
+        borderRadius:10,
+        alignItems:'center',
+        justifyContent:'center',
+
+    },
+    logoImage : {
+        resizeMode : 'stretch',
+        width: 344,
+        height: 185,
+    },
+    accountInputBox: {
+        width:192,
+        height:36,
+        borderRadius: 10,
+        borderWidth:1,
+        borderColor:'black',
+        marginBottom: 10,
+        color : 'gray',
+    },
+
+    commentForLogin: {
+        marginBottom : 20
+    },
+})
+
+export default PasswordInputScreen;

1b5ba51c8228ece9c3bbb762a325c8ac97b22ace
JangHongJoon <wkdghdwns19969@gmail.com>
2023-03-25 14:18:20 +0900

    Fix : Fixed LoginScreen.jsx bug that TextInput value not being empty

--- screens/order_related_screen/OrderSuppliesScreen.jsx
+++ screens/order_related_screen/OrderSuppliesScreen.jsx
@@ -1,26 +1,32 @@
-import {Alert, BackHandler, Image, Pressable, StyleSheet, Text, View, ScrollView} from "react-native";
+import {Alert, BackHandler, Image, Pressable, StyleSheet, Text, View, ScrollView, Modal} from "react-native";
 import React, {useEffect, useState} from "react";
 import {useNavigation} from "@react-navigation/native";
+import {getData} from "../../lib/asyncstorage";
 import {supabase} from "../../lib/supabase";
 
 
 function OrderSuppliesScreen({ navigation, route}){
     const { drawer, itemClass, itemSpecificClass, brandList } = route.params;
 
+    const [errorModalVisible, setErrorModalVisible] = useState(false);
+
     const [itemNameList, setItemNameList] = useState([]);
+    const [chargedMoney, setChargedMoney] = useState(0);
+    const [modalItemName, setModalItemName] = useState('');
+
     let tempList = [];
 
     const handleSearchItemName = async (itemClass, itemSpecificClass,brandName) => {
         const { data, error } = await supabase
             .from('supply_item_table')
-            .select('supply_item_name')
+            .select('*')
             .eq('supply_item_class',itemClass)
             .eq('supply_item_specify_class',itemSpecificClass)
             .eq('brands',brandName)
 
         if (error){
-        } else{
-
+        } else {
+            console.log(data)
             setItemNameList([...tempList])
 
             data.map(itemName => {
@@ -29,7 +35,19 @@ function OrderSuppliesScreen({ navigation, route}){
                 }
             });
             setItemNameList(tempList);
+        }
+    }
 
+    const handleChargedMoney = async (ownerId) => {
+        const { data, error } = await supabase
+            .from('shop_owner_table')
+            .select('charged_money')
+            .eq('member_id',ownerId)
+        if (error) {
+        } else {
+            // console.log(data[0].money_for_supplies)
+            setChargedMoney(data[0].charged_money)
+            // console.log(chargedMoney)
         }
     }
 
@@ -38,7 +56,10 @@ function OrderSuppliesScreen({ navigation, route}){
             return(
                 <>
                     {itemNameList.map((itemName,index) => (
-                        <Pressable key={index} style={styles.seperateDash}>
+                        <Pressable key={index} style={styles.seperateDash} onPress={() => {
+                            setModalItemName(itemName)
+                            setErrorModalVisible(true)}
+                            }>
                             <Text style={styles.label}>{itemName}</Text>
                         </Pressable>
                     ))}
@@ -66,12 +87,31 @@ function OrderSuppliesScreen({ navigation, route}){
     useEffect(() => {
         setItemNameList([])
         brandList.map(async(brandName) => await handleSearchItemName(itemClass, itemSpecificClass, brandName));
-
+        getData('owner_id').then(ownerId => {
+            handleChargedMoney(ownerId)
+        })
     }, [brandList])
 
 
     return (
         <View style={styles.container}>
+
+            <Modal
+                visible={errorModalVisible}
+                transparent={true}
+                animationType="fade"
+                onRequestClose={() => setErrorModalVisible(false)}>
+                <View style={styles.errorModalMessageContainer}>
+                    <View style={styles.errorModalMessageBox}>
+                        <Text style={{marginBottom:30, fontSize:15,}}>{modalItemName}</Text>
+                        <Pressable onPress={() => setErrorModalVisible(false)}>
+                            <Text style={{fontSize:15,}}>취소</Text>
+                        </Pressable>
+                    </View>
+                </View>
+
+            </Modal>
+
             <View style ={styles.upperComponentGroupStyle}>
                 <View style={styles.upperComponentsContainerStyle}>
                     <Image source = {require('../../images/logo.jpg')} style = {styles.logoImage} />
@@ -86,6 +126,9 @@ function OrderSuppliesScreen({ navigation, route}){
 
             {functionForMakingScrollView()}
 
+            <View style ={styles.containerForChargedMoneyStyle}>
+                <Text style={styles.label}>충전 금액 : {chargedMoney}</Text>
+            </View>
         </View>
     )
 }
@@ -101,6 +144,21 @@ const styles = StyleSheet.create({
         flex:1,
         top:10,
         position:'absolute',
+    },
+    errorModalMessageContainer: {
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor :"rgba(0,0,0,0.5)"
+    },
+    errorModalMessageBox:{
+        width:350,
+        height:200,
+        backgroundColor:"#ffffff",
+        borderRadius:10,
+        alignItems:'center',
+        justifyContent:'center',
+
     },
     upperComponentsContainerStyle: {
         flex:1,
@@ -153,13 +211,17 @@ const styles = StyleSheet.create({
         textAlign : "center",
     },
     scrollContainerStyle:{
-        flex:0.5,
+        flex:0.4,
         alignItems:'center',
         justifyContent:'center',
     },
     scrollStyle: {
         flex:0.5,
     },
+    containerForChargedMoneyStyle:{
+        top:'76%',
+        position:'absolute'
+    }
 })
 
 export default OrderSuppliesScreen;

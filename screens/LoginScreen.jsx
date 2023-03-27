4a5c70701cbb23534aa4b396391cb41cb9d41512
JangHongJoon <wkdghdwns19969@gmail.com>
2023-03-27 20:53:00 +0900

    Feat : Added 'owner_location_address' at asyncstorage

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -48,6 +48,7 @@ function LoginScreen({navigation}) {
         storeData('owner_name', data.member_name)
         storeData('owner_id', data.member_id)
         storeData('owner_brands', data.member_brands)
+        storeData('owner_location_address',data.location_address)
 
         setMemberId('')
         setMemberPassword('')

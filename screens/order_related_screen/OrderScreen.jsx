051fb873195153ef3222c976236859d8ff1c4fcb
JangHongJoon <wkdghdwns19969@gmail.com>
2023-03-28 22:30:04 +0900

    Bug : Order Piled List are not well corresponding

--- screens/order_related_screen/OrderScreen.jsx
+++ screens/order_related_screen/OrderScreen.jsx
@@ -129,6 +129,7 @@ function OrderScreen({navigation, route}){
         }else{
             if (data[0]==undefined){
                 setItemCountForBottom(0)
+                setHiddenState(false)
             }
             else{
                 setItemCountForBottom(data[0].item_count)

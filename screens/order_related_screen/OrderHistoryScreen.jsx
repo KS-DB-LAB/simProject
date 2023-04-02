4f9393cb1dfd7224ef594cabf863a2b944d845c4
JangHongJoon <wkdghdwns19969@gmail.com>
2023-04-03 00:58:51 +0900

    Feat : Made ScrollView at OrderHistoryScreen.jsx

--- screens/order_related_screen/OrderHistoryScreen.jsx
+++ screens/order_related_screen/OrderHistoryScreen.jsx
@@ -27,25 +27,40 @@ function OrderHistoryScreen ({navigation}){
 
     }
 
+    const setForItemCountExtra = (itemCountForExtra) => {
+        if (itemCountForExtra ==0 ) {
+            return ''
+        }
+        else {
+            return ' 외 ' + itemCountForExtra.toString() +'개'
+        }
+    }
 
-    const makeScrollViewForChargingHistoryList = () => {
-        //console.log(supabaseDataLocal)
+    const makeScrollViewForOrderHistoryList = () => {
         return(
             <>
                 <View style ={styles.scrollContainerStyle}>
                     <ScrollView style={styles.scrollStyle}>
-                        {supabaseDataLocal.map( chargingHistory => (
+                        {supabaseDataLocal.map( orderHistory => (
+                            //console.log(orderHistory.member_order_list[0]),
+
                             <View style={{flexDirection: 'row'}}>
-                                <View style={[styles.seperateDash,{marginRight:20, width : 100,}]}>
-                                    <Text>{new Date(chargingHistory.created_at).toLocaleString()}</Text>
+                                <View style={[styles.seperateDash,{marginRight:10, width : 100,}]}>
+                                    <Text>{new Date(orderHistory.created_at).toLocaleString()}</Text>
                                 </View>
 
-                                <View style={[styles.seperateDash,{marginRight:20, width : 150,}]}>
-                                    <Text>{numberThousandFormat(chargingHistory.requested_charging_money.toString())}원</Text>
-                                </View>
+
+                                <Pressable onPress = {() => {
+                                    setModalInnerComponents(orderHistory)
+                                    setErrorModalVisible(true)
+                                }}
+                                   style={[styles.seperateDash,{flexDirection:'row', marginRight:10, width : 180,}]}>
+                                    <Text style={{fontSize: 10}}>{orderHistory.member_order_list[0].itemName}</Text>
+                                    <Text style={{fontSize : 10}}>{setForItemCountExtra((orderHistory.member_order_list.length)-1)}</Text>
+                                </Pressable>
 
                                 <View style={[styles.seperateDash, {width : 100,}]}>
-                                    <Text>{chargingHistory.charged_status}</Text>
+                                    <Text>{orderHistory.order_status}</Text>
                                 </View>
                             </View>
                         ))}
@@ -58,7 +73,7 @@ function OrderHistoryScreen ({navigation}){
 
     const getHistoryOfChargingMoney = async (ownerId) => {
         const {data, error} = await supabase
-            .from('order_charging_table')
+            .from('order_history_table')
             .select('*')
             .eq('owner_id',ownerId)
         if (error){
@@ -69,33 +84,116 @@ function OrderHistoryScreen ({navigation}){
     }
 
     useEffect( () => {
-        //console.log('ChargingMoneySCreen - History')
+        console.log('ChargingMoneySCreen - History')
         getData('owner_id').then(ownerId => {
             getHistoryOfChargingMoney(ownerId);
         })
 
     },[isFocused])
 
+    const [createdAtForModal,setCreatedAtForModal] = useState('')
+    const [memberLocationAddressForModal,setMemberLocationAddressForModal] = useState('')
+    const [memberNameForModal,setMemberNameForModal] = useState('')
+    const [memberOrderList,setMemberOrderList] = useState([])
+    const [buyingTotalPrice, setBuyingTotalPrice] = useState(0)
+
+    const setModalInnerComponents = (orderHistory) => {
+        // console.log(orderHistory)
+        // {"created_at": "2023-04-02T08:32:32.677543+00:00", "id": 117, "member_location_address": "기장", "member_name": "장홍준", "member_order_list": [{"itemBuyingCount": 1, "itemName":
+        //  "비빔양념 1kg", "itemPrice": "8000"}], "member_order_total_price": 8000, "order_status": "발주 준비 중", "owner_id": "admin"}
+
+        setCreatedAtForModal(new Date(orderHistory.created_at).toLocaleString())
+        setMemberLocationAddressForModal(orderHistory.member_location_address)
+        setMemberNameForModal(orderHistory.member_name)
+
+        setMemberOrderList(orderHistory.member_order_list)
+
+        let tempBuyingTotalPrice = 0
+        orderHistory.member_order_list.map( orderInfo => {
+            tempBuyingTotalPrice += (orderInfo.itemPrice * orderInfo.itemBuyingCount)
+        })
+        setBuyingTotalPrice(tempBuyingTotalPrice)
+
+    }
+
+    const modalInnerComponents = () => {
+        return (
+            <>
+                <View style={{marginTop: 20,marginBottom:30, position:'absolute',top:0}}>
+                    <Text style={{fontWeight:'bold', }}>발주 기록</Text>
+                </View>
+
+                <View style={{position:'absolute',top:60, alignItems:'flex-start'}}>
+
+                    <View style={{width : '100%'}}>
+
+                        <Text style={{fontSize:10, fontWeight:'bold', marginBottom:5}}>발주 날짜 : {createdAtForModal}</Text>
+                        <Text style={{fontSize:10, fontWeight:'bold', marginBottom:5}}>위치 : {memberLocationAddressForModal}</Text>
+                        <Text style={{fontSize:10, fontWeight:'bold', marginBottom:5}}>이름 : {memberNameForModal}</Text>
+                    </View>
+                </View>
+                <View style={{width:'90%',height:'50%',top:40,alignItems:'flex-end'}}>
+                <ScrollView horizontal={true}>
+                <ScrollView>
+                    <View style={{ alignItems:'flex-end'}}>
+                    {memberOrderList.map(order => {
+                        return (
+                        <View style={{flexDirection: 'row',marginBottom:20,}}>
+                            <Text style={{marginRight:20,}}>{order.itemName}</Text>
+                            <Text style={{marginRight:20,}}>{order.itemBuyingCount}개</Text>
+                            <Text style={{marginRight:20,}}>{order.itemPrice}원</Text>
+                            <Text style={{marginRight:20,fontWeight: 'bold'}}>{order.itemPrice * order.itemBuyingCount}원</Text>
+                        </View>
+                        )
+                    })}
+                    </View>
+                </ScrollView>
+                </ScrollView>
+                </View>
+            </>
+        )
+    }
 
     const [errorModalVisible, setErrorModalVisible] = useState(false);
-    const [errorMessageModalVisible, setErrorMessageModalVisible] = useState(false)
     return (
         <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <Modal
+                visible={errorModalVisible}
+                transparent={true}
+                animationType="fade"
+                onRequestClose={() => setErrorModalVisible(false)}>
+                <View style={styles.errorModalMessageContainer}>
+                    <View style={styles.errorModalMessageBox}>
+                        {modalInnerComponents()}
 
+                        <View style={{width : '80%', alignItems:'flex-end',top:55}}>
+                            <Text style={{fontSize:15, fontWeight: 'bold'}}>총 금액 : {buyingTotalPrice}원</Text>
+                        </View>
+                        <View style={{flexDirection: 'row'}}>
+                            <Pressable onPress={() => {
+                                setErrorModalVisible(false)}}>
+                                <Text style={{fontSize:15,top:65}}>확인</Text>
+                            </Pressable>
+                        </View>
+
+                    </View>
+                </View>
+
+            </Modal>
 
             <View style ={styles.upperComponentGroupStyle}>
                 <View style={styles.upperComponentsContainerStyle}>
                     <Image source = {require('../../images/logo.jpg')} style = {styles.logoImage} />
-                    <Pressable onPress={() => drawer.openDrawer()} style={styles.sideBarIconContainerStyle}>
+                    <Pressable onPress={() => navigation.openDrawer()} style={styles.sideBarIconContainerStyle}>
                         <Image source = {require('../../images/sideBarIcon.jpg')} style = {styles.sideBarIconStyle} />
                     </Pressable>
                 </View>
                 <View style = {styles.titleContainerStyle}>
-                    <Text style ={styles.titleStyle}>금액 충전 기록</Text>
+                    <Text style ={styles.titleStyle}>발주 기록</Text>
                 </View>
             </View>
 
-            {makeScrollViewForChargingHistoryList()}
+            {makeScrollViewForOrderHistoryList()}
         </KeyboardAvoidingView>
     )
 }
@@ -180,7 +278,7 @@ const styles = StyleSheet.create({
     },
     errorModalMessageBox:{
         width:350,
-        height:200,
+        height:500,
         backgroundColor:"#ffffff",
         borderRadius:10,
         alignItems:'center',

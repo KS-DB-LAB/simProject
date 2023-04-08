b00280d18ef2dc7f43d357f9adb87b94b37d9337
JangHongJoon <wkdghdwns19969@gmail.com>
2023-04-08 12:25:02 +0900

    Refactor : Deleted function for app exit when backpress in device

--- screens/order_related_screen/OrderScreen.jsx
+++ screens/order_related_screen/OrderScreen.jsx
@@ -150,33 +150,7 @@ function OrderScreen({navigation, route}){
         }
     }
 
-
-    useEffect(() => {
-        const backAction = () => {
-            Alert.alert(
-                '종료',
-                '앱을 종료하시겠습니까?',
-                [
-                    {
-                        text: '취소',
-                        onPress: () => null,
-                        style: 'cancel',
-                    },
-                    { text: '확인', onPress: () => BackHandler.exitApp() },
-                ],
-                { cancelable: false }
-            );
-            return true;
-        };
-
-        const backHandler = BackHandler.addEventListener(
-            'hardwareBackPress',
-            backAction
-        );
-
-        return () => backHandler.remove();
-    },[])
-
+    
 
     return (
         <View style={styles.container}>
